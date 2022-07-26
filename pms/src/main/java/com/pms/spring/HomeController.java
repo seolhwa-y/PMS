f31edbe93e2cf8b3f677fc026b603ee6e9fbc9af
seolhwa-y <103579961+seolhwa-y@users.noreply.github.com>
2022-07-26 16:37:58 +0900

    Update HomeController.java
    220726 :: [ Developer UPDATE ]

--- pms/src/main/java/com/pms/spring/HomeController.java
+++ pms/src/main/java/com/pms/spring/HomeController.java
@@ -35,6 +35,7 @@ public class HomeController {
 	// 첫번째 페이지 - 로그인 페이지 :: 대쉬보드 페이지 
 	@RequestMapping(value = "/First", method = RequestMethod.GET)
 	public ModelAndView home(HttpServletRequest req, ModelAndView mav,@ModelAttribute CerB cb) {
+		/* Developer : 설화 */
 		cb.setAslPrivateIp(req.getRemoteAddr());
 		mav.addObject(cb);
 		this.cer.backController(0, mav);
@@ -44,6 +45,7 @@ public class HomeController {
 	// 회원가입 폼 이동 
 	@RequestMapping(value="/MoveJoinForm", method= RequestMethod.GET)
 	public ModelAndView joinForm(ModelAndView mav, @ModelAttribute CerB cb) {
+		/* Developer : 설화 */
 		this.cer.backController(1, mav);
 		return mav;
 	}
@@ -51,16 +53,18 @@ public class HomeController {
 	// 회원가입 제어 
 	@RequestMapping(value="/Join", method= RequestMethod.POST)
 	public ModelAndView join(HttpServletRequest req, ModelAndView mav, @ModelAttribute CerB cb) {
+		/* Developer : 설화 */
 		cb.setAslPrivateIp(req.getRemoteAddr());
 		mav.addObject(cb);
 		this.cer.backController(2, mav);
 		return mav;
 	}
-	
+
 	// 로그인 제어 
 	@RequestMapping(value = "/Auth", method = RequestMethod.POST)
 	public ModelAndView auth(HttpServletRequest req,ModelAndView mav, @ModelAttribute CerB cb) {
-		cb.setAslPrivateIp(req.getRemoteAddr()); //실제 아이피 불러오는 구문 req.getRemoteAddr
+		/* Developer : 설화 */
+		cb.setAslPrivateIp(req.getRemoteAddr());
 		mav.addObject(cb);
 		this.cer.backController(3, mav);
 		return mav;
@@ -69,6 +73,7 @@ public class HomeController {
 	// 로그아웃 제어 
 	@RequestMapping(value="/Logout", method= RequestMethod.POST)
 	public ModelAndView logOut(HttpServletRequest req, ModelAndView mav, @ModelAttribute CerB cb) {
+		/* Developer : 설화 */
 		this.cer.backController(4, mav);
 		return mav;
 	}
@@ -76,6 +81,7 @@ public class HomeController {
 	// 프로젝트 페이지 이동 
 	@RequestMapping(value="/NewProject", method= RequestMethod.POST)
 	public ModelAndView newProject(HttpServletRequest req, ModelAndView mav, @ModelAttribute CerB cb) {
+		/* Developer : 지수 */
 		this.project.backController(0, mav);
 		return mav;
 	}
@@ -83,6 +89,7 @@ public class HomeController {
 	// 프로젝트 생성 후 멤버 초대 
 	@RequestMapping(value="/InviteMemberV", method= RequestMethod.POST)
 	public ModelAndView inviteMember(HttpServletRequest req, ModelAndView mav, @ModelAttribute ProBean pro) {
+		/* Developer : 지수 */
 		mav.addObject(pro);
 		this.project.backController(1, mav);
 		System.out.println("hi");
@@ -92,6 +99,7 @@ public class HomeController {
 	// 대쉬보드 페이지 이동 
 	@RequestMapping(value="/DashBoard", method= RequestMethod.POST)
 	public ModelAndView dashBoard(HttpServletRequest req, ModelAndView mav, @ModelAttribute CerB cb) {
+		/* Developer : 채이 */
 		this.dBoard.backController(0, mav);
 		return mav;
 	}
@@ -99,6 +107,7 @@ public class HomeController {
 	// 메일로 받은 인증코드 확인 후 UPDATE
 	@RequestMapping(value="/EmailCodeCer", method= RequestMethod.POST)
 	public ModelAndView acceptMember(HttpServletRequest req, ModelAndView mav, @ModelAttribute EmailCerB ecb) {
+		/* Developer : 채이 */
 		mav.addObject(ecb);
 		this.dBoard.backController(1, mav);
 		return mav;
